497eac3ef2ab0ab2645ec0548d031cc9a65c557c
Het <amitbhai_hb@cs.iitr.ac.in>
2026-03-28 19:36:45 +0530

    updated runout.h

--- include/runout.h
+++ include/runout.h
@@ -6,24 +6,20 @@
 #include "types.h"
 #include "constants.h"
 
-extern pthread_mutex_t end_mutexes[NUM_ENDS];
+extern pthread_mutex_t end_mutexes[NUM_ENDS];   
 
 typedef struct {
     int batsman_id;
-    int requested_end; // END_1 or END_2
-    int held_end;      // end currently held
+    int requested_end;
+    int held_end;
 } resource_request;
 
 void init_runout(void);
+
 void destroy_runout(void);
 
-bool acquire_end(int batsman_id, int end_id);
-void release_end(int batsman_id, int end_id);
+int attempt_run(int striker_id, int non_striker_id, int runs);
 
-// Deadlock detection
 bool detect_deadlock(void);
-
-// Runout (kill one process) on deadlock
-void handle_runout(int batsman_id);
 
 #endif
